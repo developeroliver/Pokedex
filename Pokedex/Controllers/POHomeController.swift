7fc76b14c7da9b02e8febdfde83b4daac6772ee3
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-15 17:29:27 +0100

    remove storyboard add UICollectionViewController

---
+++ Pokedex/Controllers/POHomeController.swift
@@ -0,0 +1,17 @@
+//
+//  POHomeController.swift
+//  Pokedex
+//
+//  Created by olivier geiger on 15/03/2024.
+//
+
+import UIKit
+
+class POHomeController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .systemBackground
+        
+    }
+}
