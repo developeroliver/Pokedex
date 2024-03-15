4406cbc689cc9ff336a57b8c45e453ccac59bf00
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-15 17:41:13 +0100

    Create searchBar

--- Pokedex/Controllers/POHomeController.swift
+++ Pokedex/Controllers/POHomeController.swift
@@ -11,7 +11,26 @@ class POHomeController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewComponents()
+        
+    }
+    
+    
+    // MARK: Selectors
+    @objc func showSearchBar() {
+        print("Show searchbar")
+    }
+    
+    
+    // MARK: Helper Functions
+    func configureViewComponents() {
         collectionView.backgroundColor = .systemBackground
         
+        navigationController?.navigationBar.barTintColor    = .systemPink
+        navigationController?.navigationBar.barStyle        = .black
+        
+        navigationItem.title = "Pokédex"
+        navigationItem.rightBarButtonItem                  = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchBar))
+        navigationItem.rightBarButtonItem?.tintColor       = .secondaryLabel
     }
 }
